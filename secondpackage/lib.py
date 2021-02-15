71787d2ef32f532cd56da53d2063f6e01724df99
Helene Herman <hlne.herman@gmail.com>
2021-02-15 15:48:58 +0100

    1st version toolbox

---
+++ secondpackage/lib.py
@@ -0,0 +1,17 @@
+def how_are_you():
+    return "So well"
+
+def test_length_how_are_you():
+    assert len(how_are_you()) != 0
+
+def who_are_you():
+    return "Identities are transient"
+
+def test_who_are_you():
+    assert len(how_are_you()) !=0
+
+def you_up():
+    return "Wide awake"
+
+def you_mad():
+    return "Waiting for a revolution"
